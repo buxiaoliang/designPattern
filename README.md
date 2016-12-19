cb02aa0d1384669ef565b4b41124e625e3d558fa
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-12-19 14:08:51 +0800

    Updated doc.

---
+++ README.md
@@ -0,0 +1,6 @@
+# design pattern
+design pattern examples
+
+## Reference
+http://m.blog.csdn.net/article/details?id=8239539
+https://www.tutorialspoint.com/design_pattern/mvc_pattern.htm
--- src/adapter/AdapterPatternDemo.java
+++ src/adapter/AdapterPatternDemo.java
@@ -6,7 +6,7 @@ package adapter;
  */
 public class AdapterPatternDemo {
     public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
+        MediaPlayer audioPlayer = new AudioPlayer();
 
         audioPlayer.play("mp3", "beyond the horizon.mp3");
         audioPlayer.play("mp4", "alone.mp4");
